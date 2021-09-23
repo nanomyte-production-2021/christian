fb01430c48d45c5f3be449abb81b0a9935162559
nanomyte-production-2021 <86602743+nanomyte-production-2021@users.noreply.github.com>
2021-09-22 17:24:58 -0700

    test

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -454,30 +454,30 @@ PAGE JS
 	});
 	
 	//Demo js
-	$( window ).on( "load", function() {
-		document.onkeydown = function(e) {
-			if(e.keyCode == 123) {
-			 return false;
-			}
-			if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
-			 return false;
-			}
-			if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
-			 return false;
-			}
-			if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
-			 return false;
-			}
+	// $( window ).on( "load", function() {
+	// 	document.onkeydown = function(e) {
+	// 		if(e.keyCode == 123) {
+	// 		 return false;
+	// 		}
+	// 		if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+	// 		 return false;
+	// 		}
+	// 		if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+	// 		 return false;
+	// 		}
+	// 		if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+	// 		 return false;
+	// 		}
 		
-			if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)){
-			 return false;
-			}      
-		 }
+	// 		if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)){
+	// 		 return false;
+	// 		}      
+	// 	 }
 		 
-		$("html").on("contextmenu",function(){
-			return false;
-		});
-	});
+	// 	$("html").on("contextmenu",function(){
+	// 		return false;
+	// 	});
+	// });
 	
 	/*===================================*
 	DEMO SWITCHER JS
